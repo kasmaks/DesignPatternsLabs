03bf85e674d3164f7d93ee8e3bece2ee78565354
Kasner <kasner2001max@gmail.com>
2020-03-28 11:47:40 +0200

    lab3

--- lab3/lab3task2/src/Main.java
+++ lab3/lab3task2/src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        int budget = 7;
+        int budget = 70;
 
         Pupil p = new Pupil("Vasia", "Pupkin", "Bestiariki", "8A");
 
